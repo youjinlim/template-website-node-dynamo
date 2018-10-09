6714f70843f2c1a0962851f1005f6e21170e4d43
David <youjinlim1992@gmail.com>
2018-10-09 16:40:31 +0800

    added movies

---
+++ database/movies/Actions.js
@@ -0,0 +1,6 @@
+// CRUD Reference for AWS DynamoDB
+// https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/GettingStarted.NodeJs.03.html
+
+var crazy = function() {
+  console.log('crazyy');
+};
